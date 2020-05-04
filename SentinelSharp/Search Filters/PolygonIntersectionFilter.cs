12cf69d3e0c19358e6f1ba416b628137800769fc
abinitio <1abinitio1@gmail.com>
2020-05-04 20:19:13 +0300

    Implemented PolygonInterSectionFilter

---
+++ SentinelSharp/Search Filters/PolygonIntersectionFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SentinelSharp.Search_Filters
+{
+    class PolygonIntersectionFilter : BaseSearchFilter
+    {
+        /// <summary>
+        /// Longitudes of the polygons points
+        /// </summary>
+        public float[] PointLongitudes;
+
+        /// <summary>
+        /// Latitudes of the polygons points
+        /// </summary>
+        public float[] PointLatitudes;
+
+        public PolygonIntersectionFilter()
+        {
+
+        }
+
+        public PolygonIntersectionFilter(float[] Longitudes, float[] Latitudes)
+        {
+            PointLongitudes = Longitudes;
+            PointLatitudes = Latitudes;
+        }
+
+        public override string Build()
+        {
+            string text = "";
+            for (int i = 0; i < PointLongitudes.Length; i++)
+            {
+                text += ", " + PointLongitudes[i].ToString() + " " + PointLatitudes.ToString();
+            }
+            if (PointLatitudes.Length > 0)
+            {
+                text = text.Substring(2);
+            }
+            
+
+            return "(footprint:\"Intersects(POLYGON((" + text + ")))\")";
+        }
+    }
+}
