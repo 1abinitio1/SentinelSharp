5ffe81a894db984b95a2f41a2f3102eeaf6a538d
1abinitio1 <1abinitio1@gmail.com>
2021-04-08 18:09:16 +0100

    Clean up code a bit

--- SentinelSharp/SentinelSearch.cs
+++ SentinelSharp/SentinelSearch.cs
@@ -24,11 +24,11 @@ namespace SentinelSharp
         {
             string search = hub + Querry + "&rows=" + rows + "&start=" + (page * rows);
             string data = WebClient.DownloadString(search);
-            //Debug.Log(data);
+            
             System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
             xmlDoc.LoadXml(data);
 
-            //ProductInfo[] data
+            
             return ParseSearch(xmlDoc);
         }
 
@@ -43,34 +43,27 @@ namespace SentinelSharp
         {
             string search = hub + Querry + "&rows=" + rows + "&start=" + (page * rows);
             string data = WebClient.DownloadString(search);
-            //Debug.Log(data);
+            
             System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
             xmlDoc.LoadXml(data);
 
-            //ProductInfo[] data
             return ParseSearch(xmlDoc);
         }
 
         public ProductInfo[] ParseSearch(System.Xml.XmlDocument xmlDocument)
         {
-            //Debug.Log(xmlDocument);
             System.Xml.XmlNode xmlNode = xmlDocument.ChildNodes[1];
 
             List<ProductInfo> data = new List<ProductInfo>();
 
-            //Debug.Log(xmlNode.ChildNodes.Count);
             for (int i = 0; i < xmlNode.ChildNodes.Count; i++)
             {
-                //Debug.Log(xmlNode.ChildNodes[i].Name);
                 if (xmlNode.ChildNodes[i].Name == "entry")
                 {
-                    //Debug.Log()
                     data.Add(ParseEntry(xmlNode.ChildNodes[i]));
-                    //return null;
                 }
             }
             return data.ToArray();
-            //return null;
         }
 
         ProductInfo ParseEntry(System.Xml.XmlNode xmlNode)
@@ -78,25 +71,18 @@ namespace SentinelSharp
             ProductInfo productInfo = new ProductInfo();
             for (int i = 0; i < xmlNode.ChildNodes.Count; i++)
             {
-                //Debug.Log(xmlNode.ChildNodes[i].OuterXml);
                 if (xmlNode.ChildNodes[i].OuterXml.Contains("<link rel=\"icon\" href=\""))
                 {
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml);
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml.Substring(23,xmlNode.ChildNodes[i].OuterXml.Length-63));
                     productInfo.PreviewLink = xmlNode.ChildNodes[i].OuterXml.Substring(23, xmlNode.ChildNodes[i].OuterXml.Length - 63);
                 }
 
                 if (xmlNode.ChildNodes[i].OuterXml.Contains("<id xmlns=\"http://www.w3.org/2005/Atom\">"))
                 {
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml);
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml.Substring(40,xmlNode.ChildNodes[i].OuterXml.Length-45));
                     productInfo.Id = xmlNode.ChildNodes[i].OuterXml.Substring(40, xmlNode.ChildNodes[i].OuterXml.Length - 45);
                 }
 
                 if (xmlNode.ChildNodes[i].OuterXml.Contains("<link href=\""))
                 {
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml);
-                    //Debug.Log(xmlNode.ChildNodes[i].OuterXml.Substring(12, xmlNode.ChildNodes[i].OuterXml.Length - 52));
                     productInfo.FullDataLink = xmlNode.ChildNodes[i].OuterXml.Substring(12, xmlNode.ChildNodes[i].OuterXml.Length - 52);
                 }
             }
